20a8216f51355748478874aedcc5c1bb5fb40281
ermiyas daniel <ermiyasdaniel33@gmail.com>
2024-12-27 00:15:49 -0800

    complet signup page functionality

--- api/controlers/auth.controlers.js
+++ api/controlers/auth.controlers.js
@@ -10,7 +10,7 @@ export const signup= async (req, res, next)=>{
   const newUser =new User ({username,email,password:hashedPassword});
   try{
     await newUser.save();
-    res.status(201).json("user created successfully!")
+    res.status(201).json('user created successfully!')
 
   } catch (error){
    next(error);
