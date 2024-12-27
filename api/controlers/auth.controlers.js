cf790e6d02bf91bfcdfed91bd7cc3fdde08a3e9e
ermiyas daniel <ermiyasdaniel33@gmail.com>
2024-12-27 05:48:28 -0800

    add redux persist for current useer identification

--- api/controlers/auth.controlers.js
+++ api/controlers/auth.controlers.js
@@ -2,13 +2,14 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-
+import { errorHandler } from '../utils/error.js';
 export const signup= async (req, res, next)=>{
   // console.log(req.body);
   
   const {username, email ,password} = req.body;
   const hashedPassword = bcryptjs.hashSync(password ,10);
-  const newUser =new User ({username,email,password:hashedPassword});
+  const newUser =new User ({username,email,
+    password:hashedPassword});
   try{
     await newUser.save();
     res.status(201).json('user created successfully!')
@@ -23,7 +24,7 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404 , 'User not found!'));
+    if (!validUser) return next(errorHandler (404 , 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
